f84b4dc41469689e7fabaed5811348823379434b
Andrew Cockroft <acockroft@noggin.com.au>
2021-05-20 13:01:01 +1000

    Comment and cleanup. Plus handle currently selected line.

--- src/drawMode.ts
+++ src/drawMode.ts
@@ -1,6 +1,9 @@
 import { KeyboardHandler } from "./key.js";
 import { LatLng, Marker, MapWrapper, MapContainer } from "./map.js"
 
+/**
+ * Handles drawn items on the map.
+ */
 export class DrawMode {
     map: MapWrapper;
 
@@ -8,6 +11,7 @@ export class DrawMode {
     lines: LatLng[][] = [[]];
     mapLines: MapContainer[] =  [];
     mapMarkers: Marker[][] =  [[]];
+    currentLine: number = 0;
     colours: string[] =  [
       '#000000',
       '#0000ff',
@@ -28,6 +32,9 @@ export class DrawMode {
         this.clickListener = this.map.listen('click', function(event) { me.clickMap(event); });
     }
 
+    /**
+     * Register key handlers
+     */
     registerKeys() {
         var me = this;
         this.keyListeners.push(KeyboardHandler.register('keydown', 'Escape', me.endLine.bind(me)));
@@ -38,8 +45,8 @@ export class DrawMode {
     }
 
     /**
-     * 
-     * @param event 
+     * Handle map click
+     * @param event
      */
     clickMap(event: any) {
         var pos = event.latLng;
@@ -47,58 +54,98 @@ export class DrawMode {
     }
 
     /**
-     * 
-     * @param pos 
-     * @param i 
-     * @param j 
+     * Add a point to a line
+     * @param pos
+     * @param i
+     * @param j
      */
     addLine(pos: LatLng, i: number | false = false, j: number | false = false) {
-        if (i === false) i = this.lines.length - 1;
+        if (i === false) i = this.currentLine;
         if (j === false) j = this.lines[i].length;
-      
+
         if (j < this.lines[i].length) {
-          this.lines[i].splice(j, 0, pos);
-          this.redoMarkers(i);
+            this.lines[i].splice(j, 0, pos);
+            this.redoMarkers(i);
         } else {
-          pos = this.snapPos(pos, i, j);
-          this.lines[i].push(pos);
-          this.addLineMarker(i, j);
+            pos = this.snapPos(pos, i, j);
+            this.lines[i].push(pos);
+            this.addLineMarker(i, j);
         }
-      
+
         this.redrawLine(i);
     }
 
+    /**
+     * End a line
+     */
     endLine() {
-        this.backgroundLine(this.lines.length - 1);
-        this.lines.push([]);
+        this.backgroundLine(this.currentLine);
+        if (this.lines[this.lines.length - 1].length > 0) {
+            this.lines.push([]);
+        }
+        this.currentLine = this.lines.length - 1;
     }
 
+    /**
+     * Close and end a line
+     */
     closeLine() {
-        var i = this.lines.length - 1;
+        var i = this.currentLine;
+        var len = this.lines[i].length;
+        if (len >= 2) {
+            var a = this.lines[i][0], z = this.lines[i][len - 1];
+            if (!a.equals(z)) {
+                //Not already closed; so close.
+                this.addLine(a, false, false);
+            }
+        }
 
-        this.addLine(this.lines[i][0], false, false);
         this.endLine();
     }
 
+    /**
+     * Remove the last point from a line
+     * @returns
+     */
     undoLine() {
-        var i = this.lines.length - 1;
+        var i = this.currentLine;
         var j = this.lines[i].length - 1;
         if (j < 0) return;
-      
+
         this.mapMarkers[i][j].setMap(null);
         this.lines[i].splice(j, 1);
         this.mapMarkers[i].splice(j, 1);
         this.redrawLine(i);
     }
 
-    nextLine() {}
+    /**
+     * Switch edit mode to the next line
+     */
+    nextLine() {
+        this.backgroundLine(this.currentLine);
+        this.currentLine = (this.currentLine + 1) % this.lines.length;
+        this.foregroundLine(this.currentLine);
+    }
 
-    prevLine() {}
+    /**
+     * Switch edit mode to the previous line
+     */
+    prevLine() {
+        this.backgroundLine(this.currentLine);
+        this.currentLine = (this.currentLine + this.lines.length - 1) % this.lines.length;
+        this.foregroundLine(this.currentLine);
+    }
 
+    /**
+     * Snap the point to the nearest point which is not on the same line OR is the other end of the current line
+     * @param pos point to snap
+     * @param oi current line
+     * @param oj current point
+     * @returns the point to use
+     */
     snapPos(pos: LatLng, oi: number, oj: number): LatLng {
-        var proj = this.map.getProjection();
-        var nP = proj ? proj.fromLatLngToPoint(pos) : undefined;
-        if (!proj || !nP) return pos;
+        var nP = this.map.pointToOffset(pos);
+        if (!nP) return pos;
         var nearest = null;
         var distanceToNearest = null;
 
@@ -109,9 +156,9 @@ export class DrawMode {
                     || j == 0 && oj >= this.lines[i].length - 1 //Can snap if changing point is the last (or new last) and this is the first point
                     || j == this.lines[i].length - 1 && oj == 0; //Can snap if changing point is the first and this is the last point
 
-                var oP = proj.fromLatLngToPoint(this.lines[i][j]);
+                var oP = this.map.pointToOffset(this.lines[i][j]);
                 if (!canSnap || !oP) continue;
-      
+
                 var scale = Math.pow(2, this.map.getZoom());
                 var yDelta = Math.abs(oP.y - nP.y) * scale, xDelta = Math.abs(oP.x - nP.x) * scale;
                 var dist = Math.sqrt(yDelta * yDelta + xDelta * xDelta);
@@ -121,16 +168,21 @@ export class DrawMode {
                 }
             }
         }
-      
+
         if (nearest === null) return pos;
         return nearest;
     }
 
+    /**
+     * Redraw a particular line
+     * @param i
+     * @returns
+     */
     redrawLine(i: number) {
         if (this.mapLines[i]) this.map.remove(this.mapLines[i]);
         if (this.lines[i].length < 2) return;
         var me = this;
-      
+
         this.mapLines[i] = new MapContainer();
         if (this.lines[i][0].lat() == this.lines[i][this.lines[i].length-1].lat() && this.lines[i][0].lng() == this.lines[i][this.lines[i].length-1].lng()) {
             var poly = new google.maps.Polygon({
@@ -164,8 +216,8 @@ export class DrawMode {
     }
 
     /**
-     * 
-     * @param i 
+     * Recreate the markers on a line
+     * @param i
      */
     redoMarkers(i: number) {
         for (var ji = 0; ji < this.mapMarkers[i].length; ji++) {
@@ -178,9 +230,9 @@ export class DrawMode {
     }
 
     /**
-     * 
-     * @param i 
-     * @param j 
+     * Add a line marker
+     * @param i
+     * @param j
      */
     addLineMarker(i: number, j: number) {
         var marker = new google.maps.Marker({
@@ -196,17 +248,17 @@ export class DrawMode {
             draggable: true,
         });
         this.map.add(marker);
-        
+
         if (!this.mapMarkers[i]) this.mapMarkers[i] = [];
         this.mapMarkers[i][j] = marker;
-        
+
         var me = this;
         this.map.listenObject(marker, "click", (function(marker: any, i: number, j: number, evt: any) {
             me.lines[i].splice(j, 1);
             me.redoMarkers(i);
             me.redrawLine(i);
         }).bind(null, marker, i, j));
-        
+
         this.map.listenObject(marker, 'dragend', (function(marker: any, i: number, j: number, evt: any) {
             var pos = me.snapPos(marker.getPosition(), i, j);
             marker.setPosition(pos);
@@ -215,11 +267,20 @@ export class DrawMode {
         }).bind(null, marker, i, j));
     }
 
+    /**
+     * Bring a particular line to the foreground (edit mode)
+     * @param i
+     */
     foregroundLine(i: number) {
         this.redoMarkers(i);
         this.redrawLine(i);
     }
-      
+
+    /**
+     * Put a particular line in the background (uneditable)
+     * @param i
+     * @returns
+     */
     backgroundLine(i: number) {
         for (var j = 0; j < this.mapMarkers[i].length; j++) {
             this.mapMarkers[i][j].setMap(null);
@@ -246,7 +307,7 @@ export class DrawMode {
             this.map.add(this.mapLines[i]);
             return;
         }
-      
+
         this.mapLines[i].items.push(new google.maps.Polyline({
             path: this.lines[i],
             geodesic: true,
